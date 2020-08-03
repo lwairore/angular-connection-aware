ee1dc5948e9a237e6d168fa9d89f7d116d6f57e5
lwairore <kwairore@gmail.com>
2020-08-03 09:42:19 +0300

    - Declare ConnectionComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectionComponent } from './connection/connection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionComponent
   ],
   imports: [
     BrowserModule,
