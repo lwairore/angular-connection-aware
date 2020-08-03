ad2f84eef87fa10dd7ec67d619b400c013c5114f
lwairore <kwairore@gmail.com>
2020-08-03 09:45:25 +0300

    - Declare FastDirective.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { SlowDirective } from './slow.directive';
+import { FastDirective } from './fast.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnectionComponent,
-    SlowDirective
+    SlowDirective,
+    FastDirective
   ],
   imports: [
     BrowserModule,
