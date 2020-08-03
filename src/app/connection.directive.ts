aab121b58a5529909264995fc1b05087ee1747b4
lwairore <kwairore@gmail.com>
2020-08-03 10:26:47 +0300

    - Implement a simple directive, ConnectionDirective, that displays a different source based on the connection.

--- src/app/connection.directive.ts
+++ src/app/connection.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, Attribute, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appConnection]'
 })
-export class ConnectionDirective {
+export class ConnectionDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Attribute('slowSrc') private slowSrc,
+    @Attribute('fastSrc') private fastSrc,
+    private host: ElementRef<HTMLImageElement>
+  ) { }
+
+  ngOnInit(): void {
+    const { effectiveType } = navigator.connection;
+    let src;
+    if (
+      /\slow-2g|2g|3g/.test(effectiveType)
+    ){
+      src = this.slowSrc;
+    } else {
+      src = this.fastSrc;
+    }
+    this.host.nativeElement.setAttribute('src', src);
+  }
 
 }
